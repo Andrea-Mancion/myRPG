aa2e3a65a5f53e602464835cc37db5da883ddcaf
Andrea <andrea.mancion@epitech.eu>
2022-04-25 15:28:36 +0200

    panel inventory, just add something in the future

---
+++ inventory/print_inventory.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** print_inventory.c
+** File description:
+** print our inventory
+*/
+
+#include <stdlib.h>
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+#include <math.h>
+#include <stdio.h>
+#include "../list.h"
+#include "../my.h"
+
+#define TOTALE_CASE 27
+
+void print_inventory(struct_object *object)
+{
+    for (int a = 0; a < TOTALE_CASE; a++)
+        if (object->inventory[a]->used)
+            sfRenderWindow_drawSprite(object->window.window, object->inventory[a]->inv_s, NULL);
+}
