4797b8f8c20f4656718ecb5713f0ccab52511780
Maël <mal.gouyet@epitech.eu>
2022-05-04 16:35:43 +0200

    correct itoa so that it can accept 10

--- lib/itoa.c
+++ lib/itoa.c
@@ -14,21 +14,21 @@ static size_t count_number_number(int i)
 {
     size_t nbr = 1;
 
-    for (; i > 10; nbr++, i /= 10);
+    for (; i >= 10; nbr++, i /= 10);
     return nbr;
 }
 
 char *itoa(int i)
 {
     int nbr = 0;
-    size_t s = 0;
+    int s = 0;
     char *str = NULL;
 
     if (!i)
         return NULL;
     nbr = count_number_number(i);
     str = malloc(sizeof(char) * (nbr + 1));
-    for (size_t x = 0; i > 10; x++, i /= 10) {
+    for (size_t x = 0; i >= 10; x++, i /= 10) {
         s = i % 10;
         str[x] = s + 48;
     }
